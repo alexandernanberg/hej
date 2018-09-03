b20d8152d925e9dc9331a4bc922ba57d03c6248d
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-09-03 19:35:36 +0200

    add utils

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,13 @@
 import React from 'react'
-import Router from 'next/router'
-import cookie from 'cookie'
 import Layout from '../components/Layout'
 import { FillSpace } from '../components/StyleHelpers'
 import Loader from '../components/Loader'
 import { H1 } from '../components/Text'
-
-function parseCookies(req, options = {}) {
-  return cookie.parse(req ? req.headers.cookie || '' : document.cookie, options)
-}
-
-function redirect(context, target) {
-  if (context.res) {
-    // server
-    // 303: "See other"
-    context.res.writeHead(303, { Location: target })
-    context.res.end()
-  } else {
-    // In the browser, we just pretend like this never even happened ;)
-    Router.replace(target)
-  }
-}
+import { redirect, parseCookies } from '../lib/utils'
 
 export default class Index extends React.Component {
   static getInitialProps(context) {
-    const cookies = parseCookies(context.req)
+    const cookies = parseCookies(context)
     if (!cookies.auth) {
       redirect(context, '/login')
     }
