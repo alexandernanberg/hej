79508b9a65db231b08d315606d93c0045d26782f
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-09-03 19:35:55 +0200

    tweak document title

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Head from 'next/head'
 
-export default function Layout({ children, title = 'Hej' }) {
+export default function Layout({ children, title }) {
   return (
     <>
       <Head>
-        <title>{title}</title>
+        <title>{title ? `${title} – Hej` : 'Hej'}</title>
       </Head>
       {children}
     </>
