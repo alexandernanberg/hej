95bab8bc2336fd6a2d53174e08202b51f5d47935
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-09-08 16:22:04 +0200

    feat: use custom font awesome implementation

--- components/Icon.js
+++ components/Icon.js
@@ -1,11 +1,44 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styled from 'styled-components'
+import { findIconDefinition } from '@fortawesome/fontawesome-svg-core'
+
+const Svg = styled.svg`
+  display: inline-block;
+  vertical-align: -0.125em;
+  height: 1em;
+  overflow: visible;
+  font-size: inherit;
+  fill: currentColor;
+
+  &:not(:root) {
+    overflow: visible;
+  }
+`
 
 export default function Icon({ glyph, ...rest }) {
-  return <FontAwesomeIcon icon={glyph} {...rest} />
+  if (!glyph) return null
+
+  const {
+    icon: [width, height, , , path],
+  } = findIconDefinition(
+    typeof glyph === 'string'
+      ? {
+          iconName: glyph,
+        }
+      : glyph,
+  )
+
+  return (
+    <Svg viewBox={`0 0 ${width} ${height}`} aria-hidden {...rest}>
+      <path d={path} />
+    </Svg>
+  )
 }
 
 Icon.propTypes = {
-  glyph: PropTypes.string.isRequired,
+  glyph: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.shape({ prefix: PropTypes.string, iconName: PropTypes.string }),
+  ]).isRequired,
 }
