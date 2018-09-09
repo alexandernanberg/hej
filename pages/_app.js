1194e594d09faf9d28adf6396845d602ed5600b3
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-09-09 20:00:04 +0200

    Revert "upgrade styled components"
    This reverts commit d60ee3c9dcea973d8a07922d77f56c2437222df9.

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import App, { Container } from 'next/app'
-import { GlobalStyle } from '../lib/style'
+import { injectGlobalStyle } from '../lib/style'
 import '../lib/loadIcons'
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
 
+    injectGlobalStyle()
+
     return (
       <Container>
-        <GlobalStyle />
         <Component {...pageProps} />
       </Container>
     )
