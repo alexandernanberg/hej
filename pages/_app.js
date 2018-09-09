d60ee3c9dcea973d8a07922d77f56c2437222df9
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-09-09 19:52:24 +0200

    upgrade styled components

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import App, { Container } from 'next/app'
-import { injectGlobalStyle } from '../lib/style'
+import { GlobalStyle } from '../lib/style'
 import '../lib/loadIcons'
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
 
-    injectGlobalStyle()
-
     return (
       <Container>
+        <GlobalStyle />
         <Component {...pageProps} />
       </Container>
     )
