ef41d5857b15e816d972191cf54a8d7f63f5e16a
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-09-14 21:16:46 +0200

    add logout button

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import FillSpace from '../components/FillSpace'
+import Button from '../components/Button'
 import Loader from '../components/Loader'
+import Spacer from '../components/Spacer'
 import { H1 } from '../components/Text'
 import { redirect, parseCookies } from '../lib/utils'
+import * as auth from '../lib/auth'
 
 export default class Index extends React.Component {
   static getInitialProps(context) {
@@ -19,8 +22,16 @@ export default class Index extends React.Component {
     return (
       <Layout>
         <FillSpace>
-          <H1>Home.</H1>
+          <H1>Home</H1>
           <Loader />
+          <Spacer h={3} />
+          <Button
+            onClick={() => {
+              auth.logout()
+            }}
+          >
+            Logout
+          </Button>
         </FillSpace>
       </Layout>
     )
