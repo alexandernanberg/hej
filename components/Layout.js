2fdddcb06f10b51376a509b2eb1697164034fac2
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-09-21 20:38:33 +0200

    add global style

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Head from 'next/head'
+import { GlobalStyle } from '../lib/style'
 
 export default function Layout({ children, title }) {
   return (
     <>
+      <GlobalStyle />
       <Head>
         <title>{title ? `${title} – Hej` : 'Hej'}</title>
       </Head>
