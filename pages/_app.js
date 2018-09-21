4732b0610a254181d2e81ed983d0e26b908f61d7
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-09-21 20:37:02 +0200

    styled-components v4 + add missing packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import App, { Container } from 'next/app'
-import { injectGlobalStyle } from '../lib/style'
 import '../lib/loadIcons'
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
-
-    injectGlobalStyle()
 
     return (
       <Container>
