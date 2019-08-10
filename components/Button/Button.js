934a7fef1c6a6ca02e43725459f1ce06279ecd20
Alexander Nanberg <alex.nanberg@hotmail.com>
2019-08-10 14:36:34 +0200

    feat(button): add link functionality

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,30 +1,40 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
+import PropTypes from 'prop-types'
 import Link from 'next/link'
 import Loader from '../Loader'
 import { StyledButton } from './style'
 
-const Button = React.forwardRef(function Button(
-  { children, disabled, busy, ...props },
+const Button = forwardRef(function Button(
+  { children, href, as, disabled, busy, ...props },
   ref,
 ) {
-  return (
-    <StyledButton ref={ref} disabled={disabled || busy} {...props}>
+  const component = as || (href ? 'a' : undefined)
+
+  const button = (
+    <StyledButton
+      {...props}
+      as={component}
+      ref={ref}
+      disabled={disabled || busy}
+      aria-busy={busy}
+    >
       {busy ? <Loader gray /> : children}
     </StyledButton>
   )
-})
 
-export default Button
-
-export const LinkedButton = React.forwardRef(function LinkedButton(
-  { children, href, ...props },
-  ref,
-) {
-  return (
-    <Link href={href}>
-      <Button as="a" {...props} ref={ref}>
-        {children}
-      </Button>
+  return href ? (
+    <Link href={href} passHref>
+      {button}
     </Link>
+  ) : (
+    button
   )
 })
+
+Button.propTypes = {
+  disabled: PropTypes.bool,
+  busy: PropTypes.bool,
+  href: PropTypes.string,
+}
+
+export default Button
